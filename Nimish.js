b1c1c681ebbe531fd1ee9a00fb6195aafd1753b2
Nimish28258 <80379690+Nimish28258@users.noreply.github.com>
2024-04-07 21:37:02 +0530

    Update Nimish.js

--- Nimish.js
+++ Nimish.js
@@ -1,7 +1,4 @@
 const name = 'Nimish Malekar';
 const age = 21;
 
-// Using template literals
-console.log(My name is ${name} and I am ${age} years old.);
-
-// Output: My name is Alice and I am 22 years old.
+console.log('My name is ${name} and I am ${age} years old.');
